d711ef2eb6705a7ec04b8354cb2caed841071424
Vitor Rodarte <vitorricoy@yahoo.com.br>
2017-08-08 09:57:34 -0300

    Thread

--- CalculadoraSocket/src/br/cefetmg/lpii/calculadora/server/service/CalculadoraImpl.java
+++ CalculadoraSocket/src/br/cefetmg/lpii/calculadora/server/service/CalculadoraImpl.java
@@ -5,47 +5,53 @@
  */
 package br.cefetmg.lpii.calculadora.server.service;
 
+import br.cefetmg.lpii.calculadora.server.comunicacao.Comunicacao;
 import br.cefetmg.lpii.calculadora.model.domain.RaizesBhaskara;
 import br.cefetmg.lpii.calculadora.model.exception.ExcecaoCalculadora;
+import br.cefetmg.lpii.calculadora.model.exception.ExcecaoConexao;
 import br.cefetmg.lpii.calculadora.model.service.Calculadora;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Aluno
  */
-public class CalculadoraImpl implements Calculadora{
+public class CalculadoraImpl implements Calculadora, Runnable {
+
+    private Comunicacao com;
 
     @Override
     public double soma(double x, double y) throws ExcecaoCalculadora {
-        return x+y;
+        return x + y;
     }
 
     @Override
     public double subtracao(double x, double y) throws ExcecaoCalculadora {
-        return x-y;
+        return x - y;
     }
 
     @Override
     public double multiplicacao(double x, double y) throws ExcecaoCalculadora {
-        return x*y;
+        return x * y;
     }
 
     @Override
     public double divisao(double x, double y) throws ExcecaoCalculadora {
-        return x/y;
+        return x / y;
     }
 
     @Override
     public Double[][] multiplicacaoMatriz(Double[][] a, Double[][] b) throws ExcecaoCalculadora {
         Double[][] res = new Double[a.length][b[0].length];
-        for(int I=0; I<res.length; I++){
-            for(int J=0; J<res[0].length; J++){
-                res[I][J]=new Double(0);
+        for (int I = 0; I < res.length; I++) {
+            for (int J = 0; J < res[0].length; J++) {
+                res[I][J] = new Double(0);
             }
         }
-        for (int I=0; I<a.length; I++) {
-            for (int J=0; J<b[0].length; J++) {
-                for (int K=0; K<a[0].length; K++) {
+        for (int I = 0; I < a.length; I++) {
+            for (int J = 0; J < b[0].length; J++) {
+                for (int K = 0; K < a[0].length; K++) {
                     res[I][J] += a[I][K] * b[K][J];
                 }
             }
@@ -55,14 +61,81 @@ public class CalculadoraImpl implements Calculadora{
 
     @Override
     public RaizesBhaskara bhaskara(double a, double b, double c) throws ExcecaoCalculadora {
-        double delta = b*b + (-4*a*c);
-        if(delta>0){
-            double x1 = ((-1*b)+Math.sqrt(delta))/(2*a);
-            double x2 = ((-1*b)-Math.sqrt(delta))/(2*a);
+        double delta = b * b + (-4 * a * c);
+        if (delta > 0) {
+            double x1 = ((-1 * b) + Math.sqrt(delta)) / (2 * a);
+            double x2 = ((-1 * b) - Math.sqrt(delta)) / (2 * a);
             return new RaizesBhaskara(x1, x2, true);
-        }else{
+        } else {
             return new RaizesBhaskara(0, 0, false);
         }
     }
-    
+
+    public Comunicacao getCom() {
+        return com;
+    }
+
+    public void setCom(Comunicacao com) {
+        this.com = com;
+    }
+
+    @Override
+    public void run() {
+        try {
+            int op = (Integer) com.receberObjeto();
+            double a, b, c;
+            switch (op) {
+                case 1:
+                    a = (Double) com.receberObjeto();
+                    b = (Double) com.receberObjeto();
+                    com.enviarObjeto(soma(a, b));
+                    break;
+                case 2:
+                    a = (Double) com.receberObjeto();
+                    b = (Double) com.receberObjeto();
+                    com.enviarObjeto(subtracao(a, b));
+                    break;
+                case 3:
+                    a = (Double) com.receberObjeto();
+                    b = (Double) com.receberObjeto();
+                    com.enviarObjeto(multiplicacao(a, b));
+                    break;
+                case 4:
+                    a = (Double) com.receberObjeto();
+                    b = (Double) com.receberObjeto();
+                    com.enviarObjeto(divisao(a, b));
+                    break;
+                case 5:
+                    Double[][] matrizA,
+                            matrizB;
+                    matrizA = (Double[][]) com.receberObjeto();
+                    matrizB = (Double[][]) com.receberObjeto();
+                    for (int I = 0; I < matrizA.length; I++) {
+                        for (int J = 0; J < matrizA[0].length; J++) {
+                            System.out.print(matrizA[I][J] + " ");
+                        }
+                        System.out.println("");
+                    }
+                    for (int I = 0; I < matrizB.length; I++) {
+                        for (int J = 0; J < matrizB[0].length; J++) {
+                            System.out.print(matrizB[I][J] + " ");
+                        }
+                        System.out.println("");
+                    }
+                    Double[][] res = multiplicacaoMatriz(matrizA, matrizB);
+                    com.enviarObjeto(res);
+                    break;
+                case 6:
+                    a = (Double) com.receberObjeto();
+                    b = (Double) com.receberObjeto();
+                    c = (Double) com.receberObjeto();
+                    com.enviarObjeto(bhaskara(a, b, c));
+                    break;
+            }   
+            com.fecharConexao();
+        } catch (ExcecaoConexao | ExcecaoCalculadora ex) {
+            System.out.println("Erro: " + ex.getMessage());
+        }
+    }
+
 }
--- CalculadoraSocket/src/br/cefetmg/lpii/calculadora/server/view/Servidor.java
+++ CalculadoraSocket/src/br/cefetmg/lpii/calculadora/server/view/Servidor.java
@@ -5,9 +5,7 @@
  */
 package br.cefetmg.lpii.calculadora.server.view;
 
-import br.cefetmg.lpii.calculadora.model.exception.ExcecaoCalculadora;
 import br.cefetmg.lpii.calculadora.model.exception.ExcecaoConexao;
-import br.cefetmg.lpii.calculadora.model.service.Calculadora;
 import br.cefetmg.lpii.calculadora.server.comunicacao.Comunicacao;
 import br.cefetmg.lpii.calculadora.server.service.CalculadoraImpl;
 
@@ -17,62 +15,17 @@ import br.cefetmg.lpii.calculadora.server.service.CalculadoraImpl;
  */
 public class Servidor {
     public static void main(String[] args){
-        System.out.println("Esperando conexão");
-        Calculadora calc = new CalculadoraImpl();
-        try {
-            Comunicacao com = new Comunicacao();
-            int op = (Integer) com.receberObjeto();
-            double a, b, c;
-            switch(op){
-                case 1:
-                    a = (Double)com.receberObjeto();
-                    b = (Double)com.receberObjeto();
-                    com.enviarObjeto(calc.soma(a, b));
-                    break;
-                case 2:
-                    a = (Double)com.receberObjeto();
-                    b = (Double)com.receberObjeto();
-                    com.enviarObjeto(calc.subtracao(a, b));
-                    break;
-                case 3:
-                    a = (Double)com.receberObjeto();
-                    b = (Double)com.receberObjeto();
-                    com.enviarObjeto(calc.multiplicacao(a, b));
-                    break;
-                case 4:
-                    a = (Double)com.receberObjeto();
-                    b = (Double)com.receberObjeto();
-                    com.enviarObjeto(calc.divisao(a, b));
-                    break;
-                case 5:
-                    Double[][] matrizA, matrizB;
-                    matrizA = (Double[][])com.receberObjeto();
-                    matrizB = (Double[][])com.receberObjeto();
-                    for(int I=0; I<matrizA.length; I++){
-                        for(int J=0; J<matrizA[0].length; J++){
-                            System.out.print(matrizA[I][J]+ " ");
-                        }
-                        System.out.println("");
-                    }
-                    for(int I=0; I<matrizB.length; I++){
-                        for(int J=0; J<matrizB[0].length; J++){
-                            System.out.print(matrizB[I][J]+ " ");
-                        }
-                        System.out.println("");
-                    }
-                    Double[][] res = calc.multiplicacaoMatriz(matrizA, matrizB);
-                    com.enviarObjeto(res);
-                    break;
-                case 6:
-                    a = (Double)com.receberObjeto();
-                    b = (Double)com.receberObjeto();
-                    c = (Double)com.receberObjeto();
-                    com.enviarObjeto(calc.bhaskara(a, b, c));
-                    break;
+        while(true){
+            try {
+                System.out.println("Esperando conexão");
+                CalculadoraImpl calc = new CalculadoraImpl();
+                Comunicacao com = new Comunicacao();
+                calc.setCom(com);
+                Thread requisicao = new Thread(calc);
+                requisicao.start();
+            } catch (ExcecaoConexao ex) {
+                System.out.println("Erro: " + ex.getMessage());
             }
-            com.fecharConexao();
-        } catch (ExcecaoConexao | ExcecaoCalculadora ex) {
-            System.out.println("Erro: " + ex.getMessage());
         }
         
     }
