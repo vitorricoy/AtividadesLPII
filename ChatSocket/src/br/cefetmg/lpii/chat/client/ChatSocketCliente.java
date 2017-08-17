2c238b7128ad12d98821f66f35c9ae86cba321ce
vitorricoy <vitorricoy@yahoo.com.br>
2017-08-17 10:33:56 -0300

    ChatSocket

---
+++ ChatSocket/src/br/cefetmg/lpii/chat/client/ChatSocketCliente.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.cefetmg.lpii.chat.client;
+
+/**
+ *
+ * @author Aluno
+ */
+public class ChatSocketCliente {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
